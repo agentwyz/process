73ee1bc6dbb44373e5d6e1b2ddfbc2cf2dd0befb
agentwyz <2012663060@qq.com>
2023-09-18 22:10:52 +0800

    rename the project and write a bucket

---
+++ ch5/bucket.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+typedef char* string;
+
+struct bucket {
+    string key;
+    void* binding;
+    struct bucket *next;
+}
+
+#define SIZE 109;
+
+struct bucket* table[SIZE];
+
+unsigned int hash(char* s0) {
+    
+    unsigned int h = 0; char* s;
+    
+    for (s = s0; *s; s ++) {
+        h = h*65599 + *s;
+    }
+
+    return h;
+}
+
+struct bucket* Bucket(string key, void* binding, struct bucket* next)
+{
+    struct bucket* b = checked_malloc(sizeof(*b));
+    b->key = key;
+    b->binding = binding;
+    b->next = next;
+    
+    return b;
+}
+
+void insert(string key, void* binding) {
+    int index = hash(key) % SIZE;
+    //可以指向自已, b->a->nil
+    table[index] = Bucket(key, binding, table[index]);
+}
+
+void* lookup(string key) {
+    int index = hash(key) % SIZE;
+    
+    struct bucket* b = NULL;
+    
+    for (b = table[index]; b; b = b->next) {
+        if (0 == strcmp(b->key, key)) {
+            return b->binding;
+        }
+    }
+
+    return NULL;
+}
+
+void pop() {
+    int index = hash(key) % SIZE;
+    table[index] = table[index]->next;
+}
